51301e42d35750ea1203d3001c35def5b851e72b
Tronikelis <contactdonatas@gmail.com>
2024-01-18 17:51:20 +0200

    init:

---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = {
+    plugins: ["prettier-plugin-tailwindcss"],
+
+    tailwindConfig: "./frontend/tailwind.config.cjs",
+    tailwindFunctions: ["tv", "clsx"],
+
+    tabWidth: 4,
+    semi: true,
+    endOfLine: "auto",
+    arrowParens: "avoid",
+    printWidth: 95,
+    singleQuote: false,
+    bracketSameLine: false,
+    trailingComma: "es5",
+};
